30512d0d9779c4cfcbe704b5b26cac11c33e93a0
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-03-07 18:38:23 -0300

    BlazorShop

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(60),MinLength(3, ErrorMessage = "Title must have between 3 and 60 characters")]
+    public string Title { get; set; } = string.Empty;
+}
